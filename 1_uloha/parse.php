9e6754d2eb7d4b31894efbcd9caa7bcf324e4f4a
Norbi <Tomi.Ebik@seznam.cz>
2023-02-11 22:28:34 +0100

    Main file added

---
+++ 1_uloha/parse.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($argc == 2) {
+    if ($argv[1] == "--help") {
+        printf("Hello\n");
+        return 0;
+    }
+    // } else {
+    //     printf("Fuck\n");
+    //     return 10;
+    // }
+}
